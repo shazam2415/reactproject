9ecbf0680cd113c0daf31d1d220e5cc3907dc2a9
shazam2415 <kocisa513@gmail.com>
2025-10-16 00:52:39 +0300

    rounded yok

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,31 +9,31 @@ function PostCard({ post }) {
   return (
   <div className="group">
       <div className="lg:w-64 w-72 h-96 perspective-1000">
-        <div className={`relative w-full h-full transition-transform duration-700 transform-style-3d border-2 border-gray-200 rounded-2xl
+        <div className={`relative w-full h-full transition-transform duration-700 transform-style-3d border-2 border-gray-200
         ${isFlipped ? 'rotate-y-180' : '' }`}
         style={{ transformStyle: 'preserve-3d' }}
         >
           {/* Ön Yüz */}
           <div 
-            className="absolute w-full h-full backface-hidden bg-transparent rounded-2xl shadow-md hover:shadow-2xl transition-all duration-500 p-4 flex flex-col items-center justify-center"
+            className="absolute w-full h-full backface-hidden bg-transparent shadow-md hover:shadow-2xl transition-all duration-500 p-4 flex flex-col items-center justify-center"
             style={{ backfaceVisibility: 'hidden' }}
           >
              <span 
-            className={`absolute top-4 right-4 inline-block px-3 py-1 text-sm font-semibold text-white rounded-full
+            className={`absolute top-4 right-4 inline-block px-3 py-1 text-sm font-semibold text-white
               ${status === 'Kayıp' ? 'bg-red-500' : 'bg-green-500'}
             `}
           >
             {status}
           </span>
 
-          <span className="absolute top-4 left-4 inline-block text-2xl font-semibold rounded-full cursor-pointer">
+          <span className="absolute top-4 left-4 inline-block text-2xl font-semibold cursor-pointer">
             {name || 'Evcil Hayvan'}
           </span>
 
               <img 
             src={imageUrl} 
             alt={name} 
-            className="w-full h-48 object-cover rounded-2xl bg-gray-200 group-hover:scale-105 transition-all duration-300"
+            className="w-full h-48 object-cover bg-gray-200 group-hover:scale-105 transition-all duration-300"
             onError={(e) => {
             e.target.onerror = null; // Sonsuz döngüyü önlemek için
             e.target.src = '/default.png'; // Hata durumunda varsayılan resme geç
@@ -48,29 +48,21 @@ function PostCard({ post }) {
 
           {/* Arka Yüz */}
           <div 
-            className="absolute w-full h-full backface-hidden bg-transparent rounded-2xl shadow-2xl p-8 flex flex-col items-center justify-start rotate-y-180 gap-4"
+            className="absolute w-full h-full backface-hidden bg-transparent shadow-2xl p-8 flex flex-col items-center justify-start rotate-y-180 gap-4"
             style={{ 
               backfaceVisibility: 'hidden',
               transform: 'rotateY(180deg)'
             }}
           >
             <span 
-            className="absolute top-2 right-2 inline-block text-sm font-semibold rounded-full cursor-pointer"
+            className="absolute top-2 right-2 inline-block text-sm font-semibold cursor-pointer"
             onClick={() => setIsFlipped(!isFlipped)}
             >
 
             <X />
           </span>
            <div className="flex flex-row gap-2 items-center justify-center w-full">
-            <img 
-            src={imageUrl || '/default.png'} 
-            alt={name || 'evcil hayvan'} 
-            className="h-24 aspect-square object-cover rounded-full bg-gray-200"
-            onError={(e) => {
-            e.target.onerror = null; // Sonsuz döngüyü önlemek için
-            e.target.src = '/default.png'; // Hata durumunda varsayılan resme geç
-          }}
-            />
+            
             <div className="flex flex-col items-start justify-center w-full">
                 <h2 className="text-3xl font-bold text-gray-800">{name}</h2>
                 <p>{city}/{district}</p>
